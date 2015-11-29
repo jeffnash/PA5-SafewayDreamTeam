99aafab9f0b9c983e24a785069fa6aafa40a9373
jeffnash <jmnash@berkeley.edu>
2015-11-29 11:57:35 -0800

    lt and leq

--- PA5/cool-tree.java
+++ PA5/cool-tree.java
@@ -1309,16 +1309,63 @@ class lt extends Expression {
       * @param s the output stream 
       * */
     public void code(PrintStream s) {
-            // e1.code(s);
-            // CgenSupport.emitPush(CgenSupport.ACC, s);
-            // e2.code(s);
-            // CgenSupport.emitLoad(CgenSupport.T1, 4, CgenSupport.SP, s);
-            // CgenSupport.emitBlt(CgenSupport.ACC, CgenSupport.T1, 1, s);
-            // CgenSupport.emitLoadBool(CgenSupport.ACC, BoolConst.falsebool, s);
-            // //CgenSupport.emitJal(0, s);
-            // CgenSupport.emitLabelDef(1, s);
-            // CgenSupport.emitLoadBool(CgenSupport.ACC, BoolConst.truebool, s);
-            // CgenSupport.emitLabelDef(0, s);
+            e1.code(s);
+            CgenSupport.emitPush(CgenSupport.ACC, s);                       // pupupupupupupupupup
+            e2.code(s);
+            CgenSupport.emitLoad(CgenSupport.T1, 4 / 4, CgenSupport.SP, s);
+            CgenSupport.emitAddiu(CgenSupport.SP, CgenSupport.SP, 4, s);     // popopopopopopopopop
+            CgenSupport.emitMove(CgenSupport.T2, CgenSupport.ACC, s);
+            CgenSupport.emitLoadBool(CgenSupport.ACC, BoolConst.truebool, s);
+            CgenSupport.emitLoadBool(CgenSupport.A1, BoolConst.falsebool, s);
+
+            //DISREGARD THE COMMENTED OUT CODE! Not necessary because we are guaranteed to have typechecked to int 
+            //THANKS JAESEO :D - Jeff 11/29/15
+            //CgenSupport.emitJal("equality_test", s);
+
+           /* lessthanequality_test:          # ops in $t1 $t2
+                            # true in A0, false in A1
+                            # assume $t1, $t2 are not equal*/
+                //beq $t1 $zero _eq_false # $t2 can't also be void
+                            /*
+                CgenSupport.emitBeq(CgenSupport.T1, CgenSupport.ZERO, "_eq_false", s);
+                //beq     $t2 $zero _eq_false # $t1 can't also be void
+                CgenSupport.emitBeq(CgenSupport.T2, CgenSupport.ZERO, "_eq_false", s);                
+                //lw  $v0 0($t1)  # get tags
+                CgenSupport.emitLoad("$v0", CgenSupport.T1, 0, s);
+                //lw  $v1 0($t2)
+                CgenSupport.emitLoad("$v1", CgenSupport.T2, 0, s);
+                //bne $v1 $v0 _leq_false   # compare tags
+                CgenSupport.emitBne("$v1", "$v0", "_leq_false", s);
+                //lw  $a2 _int_tag    # load int tag <-- no offset here for some reason, so doing this instead of the below line:
+                //CgenSupport.emitLoad("$a2", "_int_tag", 0, s);
+                s.println(LW + "$a2" + " " + "(" + "_int_tag" + ")");
+                //ble $v1 $a2 _leq_int   # Integers
+                CgenSupport.emitBleq("$v1", "$a2", "_leq_int", s);
+                //_leq_int:  # handles booleans and ints
+                s.println("_leq_int" + CgenSupport.LABEL);
+
+
+                //lw  $v0,12($t1) # load values */
+                    CgenSupport.emitLoad("$v0", CgenSupport.T1, 3, s);
+                //lw  $v1,12($t2)
+                    CgenSupport.emitLoad("$v1", CgenSupport.T2, 3, s);
+                //bleq $v1 $v0 _leq_true
+                    int _lt_false_labelIndex = GlobalData.getLabelIndex(); 
+                    int _lt_true_labelIndex = GlobalData.getLabelIndex(); 
+                    CgenSupport.emitBlt("$v1", "$v0", _lt_true_labelIndex, s);
+                //_leq_false:
+                s.println(_lt_false_labelIndex + CgenSupport.LABEL);
+                    //move    $a0 $a1     # move false into accumulator
+                    CgenSupport.emitMove(CgenSupport.ACC, CgenSupport.A1, s);
+                    //jr  $ra*/
+                    CgenSupport.emitReturn(s);
+                
+                //_leq_true:
+                s.println(_lt_true_labelIndex + CgenSupport.LABEL);
+                    //jr  $ra     # return true*/
+                    CgenSupport.emitReturn(s);
+
+
     }
 
 
@@ -1431,12 +1478,16 @@ class leq extends Expression {
             CgenSupport.emitMove(CgenSupport.T2, CgenSupport.ACC, s);
             CgenSupport.emitLoadBool(CgenSupport.ACC, BoolConst.truebool, s);
             CgenSupport.emitLoadBool(CgenSupport.A1, BoolConst.falsebool, s);
-            CgenSupport.emitJal("equality_test", s);
+
+            //DISREGARD THE COMMENTED OUT CODE! Not necessary because we are guaranteed to have typechecked to int 
+            //THANKS JAESEO :D - Jeff 11/29/15
+            //CgenSupport.emitJal("equality_test", s);
 
            /* lessthanequality_test:          # ops in $t1 $t2
                             # true in A0, false in A1
                             # assume $t1, $t2 are not equal*/
                 //beq $t1 $zero _eq_false # $t2 can't also be void
+                            /*
                 CgenSupport.emitBeq(CgenSupport.T1, CgenSupport.ZERO, "_eq_false", s);
                 //beq     $t2 $zero _eq_false # $t1 can't also be void
                 CgenSupport.emitBeq(CgenSupport.T2, CgenSupport.ZERO, "_eq_false", s);                
@@ -1453,18 +1504,28 @@ class leq extends Expression {
                 CgenSupport.emitBleq("$v1", "$a2", "_leq_int", s);
                 //_leq_int:  # handles booleans and ints
                 s.println("_leq_int" + CgenSupport.LABEL);
-                //lw  $v0,12($t1) # load values
-                CgenSupport.emitLoad("$v0", CgenSupport.T1, 3, s);
+
+
+                //lw  $v0,12($t1) # load values */
+                    CgenSupport.emitLoad("$v0", CgenSupport.T1, 3, s);
                 //lw  $v1,12($t2)
-                CgenSupport.emitLoad("$v1", CgenSupport.T2, 3, s);
-                //bgt $v1 $v0 _leq_false 
-                CgenSupport.emitBleq(String src1, String src2, int label, PrintStream s);
-/*
-            _leq_true:
-                jr  $ra     # return true
-            _leq_false:
-                move    $a0 $a1     # move false into accumulator
-                jr  $ra*/
+                    CgenSupport.emitLoad("$v1", CgenSupport.T2, 3, s);
+                //bleq $v1 $v0 _leq_true
+                    int _leq_false_labelIndex = GlobalData.getLabelIndex(); 
+                    int _leq_true_labelIndex = GlobalData.getLabelIndex(); 
+                    CgenSupport.emitBleq("$v1", "$v0", _leq_true_labelIndex, s);
+                //_leq_false:
+                s.println(_leq_false_labelIndex + CgenSupport.LABEL);
+                    //move    $a0 $a1     # move false into accumulator
+                    CgenSupport.emitMove(CgenSupport.ACC, CgenSupport.A1, s);
+                    //jr  $ra*/
+                    CgenSupport.emitReturn(s);
+                
+                //_leq_true:
+                s.println(_leq_true_labelIndex + CgenSupport.LABEL);
+                    //jr  $ra     # return true*/
+                    CgenSupport.emitReturn(s);
+
     }
 
 
